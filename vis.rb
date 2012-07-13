fa72d8e13fad3abb471562893bf120f727578181
Andrey Listochkin <andrew.listochkin@gmail.com>
2012-07-13 17:03:34 +0300

    visualizer initial version, TODO: handle input

---
+++ vis.rb
@@ -0,0 +1,32 @@
+require_relative './lib/model/map.rb'
+
+map = nil;
+path = '';
+
+def read_map
+	map = Map.new('./maps/contest1.map.txt')
+	puts map.inspect
+	map
+
+end
+
+def print_map
+	gets.chomp
+	map.inspect
+end
+
+def read_step
+	step = gets
+	puts step
+	path = path + step
+	step
+end
+
+read_map
+while true
+	step = read_step
+	if step == "A"
+		break;
+	end
+	print_map
+end
