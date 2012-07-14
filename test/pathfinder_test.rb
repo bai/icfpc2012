f3c530067a6571edb9d99ddfc07878606062b480
Andrey Listochkin <andrew.listochkin@gmail.com>
2012-07-14 17:26:20 +0300

    Coords To Actions

--- test/pathfinder_test.rb
+++ test/pathfinder_test.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class PathFinderTest < Test::Unit::TestCase
+
   def test_wave
     map1_string = <<EOS
 ####L
@@ -19,5 +20,25 @@ EOS
     pf1 = Icfpc2012::PathFinder.new(map1)
     pf1.do_wave
     #pf1.trace_distmap
+  end
+
+  def test_coords_to_actions
+    coords = [
+      [0, 0], [0, 1], [1, 1], [1, 0], [1, 0], [0, 0]
+    ]
+
+    path = Icfpc2012::CoordHelper.coords_to_actions coords
+
+    assert(path == 'URDWL')
+  end
+
+  def test_coords_to_actions_invalid
+    coords = [
+      [0, 0], [1, 1]
+    ]
+
+    assert_raise(RuntimeError) do 
+      Icfpc2012::CoordHelper.coords_to_actions coords
+    end
   end
 end
