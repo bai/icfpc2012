c02e06b58d6f1db8b5889c715d6245e0105cfc3a
Sergey REDKO <sergredko@gmail.com>
2012-07-14 19:07:02 +0200

    add policies to pathfinder, way better than stupid boolean

--- test/pathfinder_test.rb
+++ test/pathfinder_test.rb
@@ -10,7 +10,7 @@ class PathFinderTest < Test::Unit::TestCase
 EOS
     map1 = Icfpc2012::Map.new(map1_string)
     pf1 = Icfpc2012::PathFinder.new(map1)
-    pf1.do_wave(map1.robot.position, false)
+    pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::MIND_ROCKS)
     assert_equal(pf1.get_shortest_dist_to([1, 1]), 1)
   end
 
@@ -21,7 +21,7 @@ EOS
   def test_shortest_path
     map1 = Icfpc2012::Map.new(get_mapfile('contest6.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
-    pf1.do_wave(map1.robot.position, false)
+    pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::MIND_ROCKS)
     #pf1.print_distmap
     assert_equal(pf1.trace_shortest_path_to(pf1.enum_closest_lambdas[10]).size,
                  pf1.get_shortest_dist_to(pf1.enum_closest_lambdas[10]) + 1)
@@ -30,9 +30,19 @@ EOS
   def test_commands_path
     map1 = Icfpc2012::Map.new(get_mapfile('contest3.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
-    pf1.do_wave(map1.robot.position, false)
+    pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::MIND_ROCKS)
     #pf1.print_distmap
     assert_equal("RRRDDDDDLLLLLD",
                  Icfpc2012::CoordHelper.coords_to_actions(pf1.trace_shortest_path_to(pf1.enum_closest_lambdas[3])))
   end
+
+  def test_safe_path
+    map1 = Icfpc2012::Map.new(get_mapfile('avoidrocks.map'))
+    pf1 = Icfpc2012::PathFinder.new(map1)
+    pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::STAY_AWAY_FROM_ROCKS)
+    #pf1.print_distmap
+    assert_equal("RUUURURRRDDDD",
+                 Icfpc2012::CoordHelper.coords_to_actions(pf1.trace_shortest_path_to(pf1.enum_closest_lambdas[0])))
+  end
+
 end
