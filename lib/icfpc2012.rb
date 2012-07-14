7eacfeb26aadc5beab4f8c5cd7d56a3e3f7d927b
e-vasyl <vae@ciklum.com>
2012-07-14 19:08:58 +0300

    fix merge

--- lib/icfpc2012.rb
+++ lib/icfpc2012.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require 'rubygems'
 require 'bundler/setup'
 
@@ -12,23 +11,6 @@ require 'icfpc2012/array_helper'
 require 'icfpc2012/gga4r_main'
 require 'icfpc2012/ga'
 require 'icfpc2012/pathfinder'
-
-module Icfpc2012
-=======
-require 'rubygems'
-require 'bundler/setup'
-
-require 'icfpc2012/map'
-require 'icfpc2012/waypoint'
-require 'icfpc2012/waypoint_path'
-require 'icfpc2012/map_rock_fall'
-require 'icfpc2012/robot'
-require 'icfpc2012/array_helper'
-require 'icfpc2012/gga4r_main'
-require 'icfpc2012/ga'
-require 'icfpc2012/pathfinder'
 require 'icfpc2012/coord_helper'
-
 module Icfpc2012
->>>>>>> 0af53108c25599872d2d45bc86166c80a9f15b36
 end
