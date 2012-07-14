3d4a425a106eb840fa07530ef0bf08c8baf1ce97
Victor Sergienko <singalen@gmail.com>
2012-07-14 20:50:52 +0300

    Patterns DSL skeleton by salpaev

---
+++ test/patterns_defs_test.rb
@@ -0,0 +1,34 @@
+require File.expand_path('../test_helper', __FILE__)
+
+class DSLTest < Test::Unit::TestCase
+  def test_simple
+  pattern_def = <<EOS1
+define_pattern ("simple1")
+body <<EOS
+xpx
+#*#
+?.p
+EpE
+EOS
+path "LRTD"
+path "LRTD"
+
+define_pattern "simple2"
+body <<EOS
+xpx
+#*#
+?.p
+?pE
+EOS
+path "LRTD"
+path "LRTD"
+EOS1
+  
+  #map = Icfpc2012::Map.new
+  defs = Icfpc2012::PatternDefs.new
+  defs.load(pattern_def)
+  assert_equal(2,defs.size)
+  assert_equal('E',defs[0].get_at(0,0))
+  assert_equal('?',defs[1].get_at(0,0))
+  end
+end
