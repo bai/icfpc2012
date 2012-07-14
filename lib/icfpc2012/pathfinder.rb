f2ca418ef70f80ee286c8a5e9ce84b3eda48f940
storm <andrei.tsygankov@gmail.com>
2012-07-14 16:15:51 +0300

    + pathfinder: added option to ignore rocks in do_wave

--- lib/icfpc2012/pathfinder.rb
+++ lib/icfpc2012/pathfinder.rb
@@ -10,32 +10,32 @@ module Icfpc2012
       self.distmap = Array.new(cols) { Array.new(rows, -1) }
     end
 
-    def do_wave
+    def do_wave(x, y, ignore_rocks)
 
       newFront = []
       oldFront = []
-      oldFront.push [map.robot.y, map.robot.x]
-      distmap[map.robot.y][map.robot.x] = 0
-      t = 0;
+      oldFront.push [y, x]
+      distmap[y][x] = 0
+      t = 0
 
       while oldFront.length != 0 do
-        puts oldFront.length
+
         oldFront.each { |c|
           ri = c[0]
           ci = c[1]
-          if distmap[ri + 1][ci] == -1 && map.walkable?(ci, ri + 1)
+          if distmap[ri + 1][ci] == -1 && (map.walkable?(ci, ri + 1) || (ignore_rocks && map.get_at(ci, ri + 1) == '*'))
             distmap[ri + 1][ci] = t + 1
             newFront.push [ri + 1, ci]
           end
-          if distmap[ri - 1][ci] == -1 && map.walkable?(ci, ri - 1)
+          if distmap[ri - 1][ci] == -1 && (map.walkable?(ci, ri - 1) || (ignore_rocks && map.get_at(ci, ri - 1) == '*'))
             distmap[ri - 1][ci] = t + 1
             newFront.push [ri - 1, ci]
           end
-          if distmap[ri][ci + 1] == -1 && map.walkable?(ci + 1, ri)
+          if distmap[ri][ci + 1] == -1 && (map.walkable?(ci + 1, ri) || (ignore_rocks && map.get_at(ci + 1, ri) == '*'))
             distmap[ri][ci + 1] = t + 1
             newFront.push [ri, ci + 1]
           end
-          if distmap[ri][ci - 1] == -1 && map.walkable?(ci - 1, ri)
+          if distmap[ri][ci - 1] == -1 && (map.walkable?(ci - 1, ri) || (ignore_rocks && map.get_at(ci - 1, ri) == '*'))
             distmap[ri][ci - 1] = t + 1
             newFront.push [ri, ci - 1]
           end
@@ -44,21 +44,6 @@ module Icfpc2012
         newFront = []
         t += 1
       end
-
-=begin
-      ri = 0
-      distmap.each {|r|
-        ci = 0
-        r.each {|c|
-          if map.walkable?(ci, ri) && distmap[ri][ci] == -1
-            if distmap[ri + 1][ci] != -1 distmap[ri][ci]
-              distmap[ri][ci] = 2
-          end
-          ci+=1
-        }
-        ri+=1
-      }
-=end
     end
 
     def trace_distmap
--- test/pathfinder_test.rb
+++ test/pathfinder_test.rb
@@ -10,14 +10,14 @@ class PathFinderTest < Test::Unit::TestCase
 EOS
     map1 = Icfpc2012::Map.new(map1_string)
     pf1 = Icfpc2012::PathFinder.new(map1)
-    pf1.do_wave
+    pf1.do_wave(map1.robot.x, map1.robot.y, false)
     #pf1.trace_distmap
   end
 
   def test_wave_big
-    map1 = Icfpc2012::Map.new(File.read('../maps/contest10.map.txt'))
+    map1 = Icfpc2012::Map.new(File.read('../maps/contest6.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
-    pf1.do_wave
-    #pf1.trace_distmap
+    pf1.do_wave(map1.robot.x, map1.robot.y, true)
+    pf1.trace_distmap
   end
 end
