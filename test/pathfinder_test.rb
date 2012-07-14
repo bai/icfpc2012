8da3a43cb3aa86b65a8b7faafdf565106a4f37fa
Sergey REDKO <sergredko@gmail.com>
2012-07-14 20:29:29 +0200

    clean up st0rm's mess again

--- test/pathfinder_test.rb
+++ test/pathfinder_test.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../test_helper', __FILE__)
 
 class PathFinderTest < Test::Unit::TestCase
+  def get_mapfile(file)
+    File.read(File.join(File.dirname(__FILE__), '../maps/', file))
+  end
   def test_wave
     map1_string = <<EOS
 ####L
@@ -15,7 +18,7 @@ EOS
   end
 
   def test_shortest_path
-    map1 = Icfpc2012::Map.new(File.read('../maps/contest6.map.txt'))
+    map1 = Icfpc2012::Map.new(get_mapfile('contest6.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
     pf1.do_wave(map1.robot.position)
     #pf1.print_distmap
@@ -24,7 +27,7 @@ EOS
   end
 
   def test_commands_path
-    map1 = Icfpc2012::Map.new(File.read('../maps/contest3.map.txt'))
+    map1 = Icfpc2012::Map.new(get_mapfile('contest3.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
     pf1.do_wave(map1.robot.position)
     #pf1.print_distmap
@@ -33,7 +36,7 @@ EOS
   end
 
   def test_path_invalid
-    map1 = Icfpc2012::Map.new(File.read('../maps/contest9.map.txt'))
+    map1 = Icfpc2012::Map.new(get_mapfile('contest9.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
     pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::IGNORE_ROCKS)
     #pf1.print_distmap
@@ -44,7 +47,7 @@ EOS
   end
 
   def test_path_valid
-    map1 = Icfpc2012::Map.new(File.read('../maps/contest9.map.txt'))
+    map1 = Icfpc2012::Map.new(get_mapfile('contest9.map.txt'))
     pf1 = Icfpc2012::PathFinder.new(map1)
     pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::IGNORE_ROCKS)
     #pf1.print_distmap
@@ -53,4 +56,14 @@ EOS
     wp = Icfpc2012::WaypointPath.new(map1, path)
     assert_equal(true, wp.valid?)
   end
+
+  def test_safe_path
+    map1 = Icfpc2012::Map.new(get_mapfile('avoidrocks.map'))
+    pf1 = Icfpc2012::PathFinder.new(map1)
+    pf1.do_wave(map1.robot.position, Icfpc2012::PathFinder::STAY_AWAY_FROM_ROCKS)
+    #pf1.print_distmap
+    assert_equal("RUUURURRRDDDD",
+                 Icfpc2012::CoordHelper.coords_to_actions(pf1.trace_shortest_path_to(pf1.enum_closest_lambdas[0])))
+  end
+
 end
