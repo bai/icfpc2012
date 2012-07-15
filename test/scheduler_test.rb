b258b8693ab63f8c9b375f45552a28c8542df045
storm3d <andrei.tsygankov@gmail.com>
2012-07-15 03:28:20 +0300

    + fixed scheduler tests

--- test/scheduler_test.rb
+++ test/scheduler_test.rb
@@ -4,19 +4,18 @@ class SchedulerTest < Test::Unit::TestCase
   def get_mapfile(file)
     File.read(File.join(File.dirname(__FILE__), '../maps/', file))
   end
-=begin
+
   def test_level1
     map1 = Icfpc2012::Map.new(get_mapfile('contest1.map.txt'))
     sc = Icfpc2012::Scheduler.new(map1)
     assert_equal(sc.solve, ["DLRDDUULLLDDL", 212])
 
   end
-=end
+
   def test_level3
-    map1 = Icfpc2012::Map.new(get_mapfile('contest9.map.txt'))
+    map1 = Icfpc2012::Map.new(get_mapfile('contest3.map.txt'))
     sc = Icfpc2012::Scheduler.new(map1)
-    sc.solve
-    #assert_equal(sc.solve, ["DLRDDUULLLDDL", 212])
+    assert_equal(sc.solve, ["RDDDRRDDLLLLLRRRUULLRRDDLLLDRRURURRUR", 263])
 
   end
 
