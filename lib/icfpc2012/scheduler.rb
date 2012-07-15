ba6fec48f4bd60db517c090ceafb7e9da9edad2e
storm <andrei.tsygankov@gmail.com>
2012-07-15 21:10:35 +0300

    + scheduler with local solver and clasteriztor integration

--- lib/icfpc2012/scheduler.rb
+++ lib/icfpc2012/scheduler.rb
@@ -1,7 +1,7 @@
 module Icfpc2012
   class Scheduler
 
-    LIGHT_SEARCH_WIDTH = 3
+    LIGHT_SEARCH_WIDTH = 4
     HEAVY_SEARCH_WIDTH = 6
 
     attr_accessor :map_origin, :best_solution, :best_score, :max_iterations
@@ -13,13 +13,13 @@ module Icfpc2012
       self.max_iterations = LIGHT_SEARCH_WIDTH
     end
 
-    def process_wave(pf, map, path_so_far, score_so_far, depth)
+    def process_wave(pf, map, path_so_far, score_so_far, depth, pfMind = nil)
       #pf.print_distmap
 
       if(map.remaining_lambdas == 0)
         path = Icfpc2012::CoordHelper.coords_to_actions(pf.trace_shortest_path_to(map.lift_position))
 
-        return if(path == 'E')
+        #return if(path == 'E')
 
         wp = Icfpc2012::WaypointPath.new(map, path)
         #puts path
@@ -39,14 +39,38 @@ module Icfpc2012
       end
 
       # iterate thru
-      #interation = 0
-      lambdas = pf.enum_closest_lambdas
-      lambdas.each { |l|
+      clusters = pf.enum_closest_clusters
+      clusters.each { |cluster|
+        l = cluster[0]
+        path_coords = pf.trace_shortest_path_to(l)
 
-        path = Icfpc2012::CoordHelper.coords_to_actions(pf.trace_shortest_path_to(l))
+        if(pfMind != nil)
+          last_reachable_coord = path_coords.reverse.find { |coord|
+            pfMind.get_shortest_dist_to(coord) != -1
+            }
 
-        #next if(path == 'E')
+          if(last_reachable_coord == map.robot.position)
 
+            path_coords_remainder = path_coords.last(path_coords.size - 1)
+            #puts "Solver really needed and was called:"
+            #puts path_coords_remainder.inspect
+            #map.to_s.each_line { |line| puts line }
+
+            solution = Icfpc2012::BacktrackingSolver.repair_path(map, path_coords_remainder, 3)
+            #puts "Solver returned: "+(solution == nil ? "nil" : solution.path)
+
+            if solution != nil
+              puts "blocked solution found"
+              recurse(solution.last_map, path_so_far + solution.path, solution.last_map.score, depth + 1)
+            end
+            return
+          end
+
+          path_coords = pfMind.trace_shortest_path_to(last_reachable_coord)
+
+        end
+
+        path = Icfpc2012::CoordHelper.coords_to_actions path_coords
         wp = Icfpc2012::WaypointPath.new(map, path)
 
         #puts l.inspect
@@ -55,15 +79,30 @@ module Icfpc2012
 
         if(wp.valid?)
           recurse(wp.waypoints.last.map, path_so_far + path, wp.waypoints.last.map.score, depth + 1)
-        end
 
-        #interation+=1
-        #break if interation > max_iterations
+        else
+
+          path_remainder_coords = path_coords.last(path.size - wp.path.size)
+
+          #puts "Solver called to avoid rock:"
+          #puts path_remainder_coords.inspect
+          #wp.last_map.to_s.each_line { |line| puts line }
+
+          solution = Icfpc2012::BacktrackingSolver.repair_path(wp.last_map, path_remainder_coords)
+          #puts "Solver returned: "+(solution == nil ? "nil" : solution.path)
+
+          if solution != nil
+            puts "died solution found"
+            recurse(solution.last_map, path_so_far + path + solution.path, solution.last_map.score, depth + 1)
+          end
+
+        end
       }
     end
 
     def recurse(map, path_so_far, score_so_far, depth)
-      begin # try catch for timing exception
+
+
         if(score_so_far > self.best_score)
           self.best_score = score_so_far
           self.best_solution = path_so_far
@@ -71,37 +110,34 @@ module Icfpc2012
           puts [self.best_solution, self.best_score].inspect
         end
 
-        pf = Icfpc2012::PathFinder.new(map)
+        pfMindRocks = Icfpc2012::PathFinder.new(map)
+        pfIgnoreRocks = Icfpc2012::PathFinder.new(map)
 
-        pf.max_lambdas = max_iterations
-        pf.do_wave(map.robot.position, Icfpc2012::PathFinder::MIND_ROCKS)
-        process_wave(pf, map, path_so_far, score_so_far, depth)
+        pfMindRocks.max_clusters = max_iterations
+        pfIgnoreRocks.max_clusters = max_iterations
+        pfMindRocks.do_wave(map.robot.position, Icfpc2012::PathFinder::MIND_ROCKS)
 
-        pf.do_wave(map.robot.position, Icfpc2012::PathFinder::IGNORE_ROCKS)
-        process_wave(pf, map, path_so_far, score_so_far, depth)   
-      rescue RuntimeError => e
-        puts 'Timer called'
-        puts e.inspect
-        if(score_so_far > self.best_score)
-          self.best_score = score_so_far
-          self.best_solution = path_so_far
-          map.to_s.each_line { |line| puts line }
-          puts [self.best_solution, self.best_score].inspect
+        if(pfMindRocks.enum_closest_clusters.size != 0)
+          process_wave(pfMindRocks, map, path_so_far, score_so_far, depth)
+        else
+          pfIgnoreRocks.do_wave(map.robot.position, Icfpc2012::PathFinder::IGNORE_ROCKS)
+          process_wave(pfIgnoreRocks, map, path_so_far, score_so_far, depth, pfMindRocks)
         end
-      end
     end
 
     def solve
       #timings    
       Icfpc2012::Timer.new 150 # seconds
+      begin # try catch for timing exception
+        self.max_iterations = LIGHT_SEARCH_WIDTH
+        recurse(map_origin, "", 0, 0)
 
-      self.max_iterations = LIGHT_SEARCH_WIDTH
-      recurse(map_origin, "", 0, 0)
-
-      # we got something dirty now: try deeper
-      self.max_iterations = HEAVY_SEARCH_WIDTH
-      recurse(map_origin, "", 0, 0)
-      [best_solution, best_score]
+        # we got something dirty now: try deeper
+        #self.max_iterations = HEAVY_SEARCH_WIDTH
+        #recurse(map_origin, "", 0, 0, false)
+      ensure
+        [best_solution, best_score]
+      end
     end
   end
 end
